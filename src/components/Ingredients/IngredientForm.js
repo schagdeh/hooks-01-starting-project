07eab59213e090ad606029cf423e1874d4619a47
Phuntsok Schagdeh <phuntsok@coteries.com>
2019-09-04 08:42:23 +0200

    two way binding (input) in functional component with useState()

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  const inputState = useState({title: '', amount: ''})
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +16,27 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input type="text" id="title"
+                   value={inputState[0].title}
+                   onChange={e => {
+                     const newTitle = e.target.value
+                     inputState[1]({
+                     title: newTitle,
+                     amount: inputState[0].amount
+                   })
+                   }}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input type="number" id="amount"
+                   value={inputState[0].amount}
+                   onChange={e => {
+                     const newAmount = e.target.value
+                     inputState[1]({
+                       amount: newAmount,
+                       title: inputState[0].title
+                     })
+                   }}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
