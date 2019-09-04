114856caaa6d98b5ef884b0c11484462dc7107bc
Phuntsok Schagdeh <phuntsok@coteries.com>
2019-09-04 09:28:07 +0200

    managing state in useState() separately so all the other state doent have to be merged.

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -4,7 +4,8 @@ import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
-  const [inputState, setInputState] = useState({title: '', amount: ''})
+  const [enteredTitle, setEnteredTitle] = useState( '')
+  const [enteredAmount, setEnteredAmount] = useState( '')
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -17,26 +18,14 @@ const IngredientForm = React.memo(props => {
           <div className="form-control">
             <label htmlFor="title">Name</label>
             <input type="text" id="title"
-                   value={inputState.title}
-                   onChange={e => {
-                     const newTitle = e.target.value
-                     setInputState({
-                     title: newTitle,
-                     amount: inputState.amount
-                   })
-                   }}/>
+                   value={enteredTitle}
+                   onChange={e => setEnteredTitle(e.target.value)}/>
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
             <input type="number" id="amount"
-                   value={inputState.amount}
-                   onChange={e => {
-                     const newAmount = e.target.value
-                     setInputState({
-                       amount: newAmount,
-                       title: inputState.title
-                     })
-                   }}/>
+                   value={enteredAmount}
+                   onChange={e => setEnteredAmount(e.target.value)}/>
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
